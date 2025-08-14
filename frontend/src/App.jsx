bcf72641719f30c7a4647f5a12657b49a9b0aed1
Mohammed Shahid <shahid.mohammed@9xtechonology>
2025-08-14 11:48:14 +0530

    merge async

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,63 +1,21 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route, createBrowserRouter, RouterProvider } from 'react-router-dom';
-// import Login from './pages/login.jsx';
-// import Home from './pages/Home.jsx';
-// import Register from './pages/Register.jsx';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './Pages/login.jsx';
+import Home from './Pages/Home.jsx';
 
-// function App() {
-  
-//   const routers = createBrowserRouter([
-//   { path: "/", element: <Home /> },
-//   { path: "/login", element: <Login /> },
-//   { path: "/register", element: <Register /> },
-// ]);
-
-//   return (
-//     <RouterProvider router={routers}>
-
-//     </RouterProvider>
-//   );
-    
-// }
-
-// export default App;
-
-
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
-// import Dashboard from "./pages/Dashboard";
-
-// const router = createBrowserRouter([
-//   { path: "/", element: <Login /> },
-//   { path: "/login", element: <Login /> },
-//   { path: "/register", element: <Register /> },
-//   { path: "/dashboard", element: <Dashboard /> }, // ✅ This is important
-// ]);
-
-// export default function App() {
-//   return <RouterProvider router={router} />;
-// }
-import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import KYC from "./pages/KYC";
-import Dashboard from "./pages/Dashboard";
-
-export default function App() {
-  const [step, setStep] = useState(1);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
+function App() {
+  const routers = createBrowserRouter([
+      {
+        path: "/", element:<Home/>
+      },
+      {path: "/login", element:<Login/>}
+    ])
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/register" />} />
-        <Route path="/register" element={<Register setStep={setStep} setIsAuthenticated={setIsAuthenticated} />} />
-        <Route path="/login" element={<Login setStep={setStep} setIsAuthenticated={setIsAuthenticated} />} />
-        <Route path="/kyc" element={isAuthenticated ? <KYC setStep={setStep} /> : <Navigate to="/login" />} />
-        <Route path="/dashboard" element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" />} />
-      </Routes>
-    </Router>
+    <RouterProvider router={routers}>
+
+    </RouterProvider>
   );
+    
 }
+
+export default App;
